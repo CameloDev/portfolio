2d93d2c13d28417a70b7fb21e5d1c228e7e0ebbb
Camelo <121176469+CameloDev@users.noreply.github.com>
2025-06-06 01:26:30 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
       { protocol: "http", hostname: "localhost" },
       { protocol: "https", hostname: "ap-south-1.graphassets.com" },
       { protocol: "https", hostname: "www.camelodev.tech" },
+      { protocol: "https", hostname: "camelodev.tech" },
+      { protocol: "https", hostname: "camelodev-portfolio.vercel.app" },
     ],
   },
 };
